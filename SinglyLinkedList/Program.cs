85b50ea5bc1cff79a155469231cbf66d59aae9c4
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-16 19:02:32 +0530

    [Ritvik]Add: Welcome Message

---
+++ SinglyLinkedList/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SinglyLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked List Implementation!!!\n");
+        }
+    }
+}
