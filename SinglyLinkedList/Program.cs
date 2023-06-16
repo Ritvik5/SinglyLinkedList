e6aa6c80357a591ec79bc4563caba122e97bc737
Ritvik5 <sharma.ritvik56@gmail.com>
2023-06-16 19:14:52 +0530

    [Ritvik]Add: Using inbuilt linked list method.

--- SinglyLinkedList/Program.cs
+++ SinglyLinkedList/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace SinglyLinkedList
+﻿using System;
+using System.Collections.Generic;
+
+namespace SinglyLinkedList
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Linked List Implementation!!!\n");
+
+            LinkedList<int> linkedList = new LinkedList<int>();
+            linkedList.AddLast(56);
+            linkedList.AddLast(30);
+            linkedList.AddLast(70);
+            
+            foreach(int item in linkedList)
+            {
+                Console.WriteLine("| "+item+" |");
+            }
+
         }
     }
 }
